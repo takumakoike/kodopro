deea1910138e318f804810e1c133635fe0309cb3
takumakoike <takuma.151e@gmail.com>
2024-03-09 15:49:17 +0900

    ページ追加・カレンダー追加

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,32 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocale from '@fullcalendar/core/locales/ja'; // 追加
+
+// import './App.css';
+
+const ReactCalendar = () => {
+    return (
+        <div className='bg-white'>
+            <FullCalendar 
+                plugins={[dayGridPlugin]} 
+                initialView="dayGridMonth" 
+                locales={[jaLocale]}
+                locale="ja"
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek',
+                }}
+                themeSystem='bootstrap5'
+                events={[
+                    {title:'開催日', start: '2024-03-06'},
+                    {title:'開催日', start: '2024-03-12'},
+                    {title:'開催日', start: '2024-03-19'},
+                ]}
+            />
+
+        </div>
+    );
+}
+
+export default ReactCalendar;
